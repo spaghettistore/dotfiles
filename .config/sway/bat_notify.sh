bc20be5b13bf04c329b01655efadc4a1d0f20592
spaghettistore <spaghettistore@tutamail.com>
2025-11-23 19:17:37 +0000

    Added '--app-name' flag to bat_notify.sh

--- .config/sway/bat_notify.sh
+++ .config/sway/bat_notify.sh
@@ -2,10 +2,12 @@
 
 # Low battery notifier
 
+script_name="$(basename -- "$0")"
+
 # Kill already running processes
-already_running="$(pgrep -c 'bat_notify.sh')"
+already_running="$(pgrep -c "$script_name")"
 if [[ $already_running -gt 1 ]]; then
-    pkill -f --older 1 'bat_notify.sh'
+    pkill -f --older 1 "$script_name"
 fi
 
 while true; do
@@ -14,18 +16,18 @@ while true; do
 
     if [[ $battery_status == 'Discharging' && $battery_charge -le 25 ]]; then
         if [[ $battery_charge -le 15 ]]; then
-            notify-send --urgency=critical "Battery critically low" "${battery_charge}%"
+            notify-send --app-name="$script_name" --urgency=critical "Battery critically low" "${battery_charge}%"
             sleep 180
         else
-            notify-send --urgency=critical "Battery low" "${battery_charge}%"
+            notify-send --app-name="$script_name" --urgency=critical "Battery low" "${battery_charge}%"
             sleep 300
         fi
     elif [[ $battery_status == 'Charging' && $battery_charge -ge 80 ]]; then
         if [[ $battery_charge -ge 95 ]]; then
-            notify-send --urgency=critical "Battery full" "${battery_charge}%"
+            notify-send --app-name="$script_name" --urgency=critical "Battery full" "${battery_charge}%"
             sleep 180
         else
-            notify-send "Battery high" "${battery_charge}%"
+            notify-send --app-name="$script_name" "Battery high" "${battery_charge}%"
             sleep 300
         fi
     else
