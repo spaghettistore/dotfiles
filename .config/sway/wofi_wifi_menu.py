41ac06acdde096559d53da1fe97563e17cbc8745
spaghettistore <spaghettistore@tutamail.com>
2025-11-29 15:33:46 +0000

    Slightly cleaned up wofi_wifi_menu.py

--- .config/sway/wofi_wifi_menu.py
+++ .config/sway/wofi_wifi_menu.py
@@ -8,8 +8,17 @@ import sys
 HOME = os.environ.get("HOME")
 FILE_NAME=f"{HOME}/.config/wofi_wifi_menu/ssid_list.json"
 
+def check_if_ssid_file_exists() -> None:
+    # If file containing ssids does not exist, use nmtui instead
+    if not os.path.exists(FILE_NAME):
+        print(f"wofi_wifi_menu.py: os.path.exists: '{FILE_NAME}': No such file or directory")
+        print("Using 'nmtui' instead")
+        subprocess.run("nmtui")
+        exit()
+
+
 def get_wifi_status() -> str:
-    # Returns 'enabled' or 'disabled'
+    # This function returns 'enabled' or 'disabled'
     process = subprocess.run(
         ["nmcli", "--fields", "WIFI", "g"],
         text=True,
@@ -22,6 +31,9 @@ def get_wifi_status() -> str:
 
 
 def get_wifi_name() -> str:
+    # This function returns the name of the currently connected SSID.
+    # This function expects wifi to be enabled, else it  will reutrn the
+    # otherwise trailing line 'lo'.
     process = subprocess.run(
         ["nmcli", "-t", "-f", "NAME", "c", "show", "--active"],
         text=True,
@@ -34,12 +46,21 @@ def get_wifi_name() -> str:
     return name
 
 
-def get_wofi_response(bash_list: str) -> str:
+def get_status_prompt() -> str:
+    # Will return a str to be used as the prompt for wofi.
+    # E.g. 'enabled: SSID_NAME'
     prompt = get_wifi_status()
+
     if prompt == "enabled":
         name = get_wifi_name()
         prompt = f"{prompt}: {name}"
 
+    return prompt
+
+
+def get_wofi_response(bash_list: str, prompt: str) -> str:
+    # This function calls wofi and returns the response, or exits if wofi is
+    # quit without selecting anything.
     command = f'printf -- "{bash_list}" | wofi --matching="fuzzy" -ip "{prompt}" --show dmenu'
 
     try:
@@ -53,30 +74,26 @@ def get_wofi_response(bash_list: str) -> str:
     return response
 
 
-def check_if_ssid_file_exists() -> None:
-    # If file containing ssids does not exist, use nmtui instead
-    if not os.path.exists(FILE_NAME):
-        print(f"wofi_wifi_menu.py: os.path.exists: '{FILE_NAME}': No such file or directory")
-        print("Using 'nmtui' instead")
-        subprocess.run("nmtui")
-        exit()
-
-
 def main():
     all_args = sys.argv[1:]
     check_if_ssid_file_exists()
 
-    # Read json dictionary. key="nickname", value="SSID"
+    # Read file containing json dictionary of saved ssids. With the value being
+    # the SSID to connect to, and the key being the nickname (e.g. "home" and
+    # "ext") that will be displayed in wofi menu.
     with open(FILE_NAME, "r") as file:
         data = json.load(file)
 
-    # If no provided arguments, read selection with wofi
+    # If no external arguments provided, read selection with wofi, else use
+    # provided arg.
     if len(all_args) == 0:
-        # Convert python list to bash list
+        # Convert python list of ssid nicknames to bash list, and append 'on'
+        # and 'off' for options to toggle wifi.
         bash_list = "\n".join(data.keys())
-        # Append options 'on' and 'off'
         bash_list += "\non\noff"
-        response = get_wofi_response(bash_list)
+
+        prompt = get_status_prompt()
+        response = get_wofi_response(bash_list, prompt)
     else:
         response = all_args[0]
 
