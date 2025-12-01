d9a4ca2e9968995026d03b086252553a1b68fb27
spaghettistore <spaghettistore@tutamail.com>
2025-12-01 19:58:40 +0000

    Added nvim keybinds for ':Telescope oldfiles' and ':Telescope resume'.

--- .config/nvim/lua/config/plugins/telescope.lua
+++ .config/nvim/lua/config/plugins/telescope.lua
@@ -45,8 +45,9 @@ return {
             vim.keymap.set("n", "<leader>fg", require('telescope.builtin').git_files, {desc = ":Telescope git_files"})
             vim.keymap.set("n", "<leader>gs", require('telescope.builtin').git_status, {desc = ":Telescope git_status"})
             vim.keymap.set("n", "<leader>gS", require('telescope.builtin').git_stash, {desc = ":Telescope git_stash"})
+            vim.keymap.set("n", "<leader>sr", require('telescope.builtin').oldfiles, {desc = ":Telescope oldfiles"})
+            vim.keymap.set("n", "<leader>sR", require('telescope.builtin').resume, {desc = ":Telescope resume"})
 
-            -- Grep current open buffers
             vim.keymap.set("n", "<leader>sb", function()
                 require('telescope.builtin').grep_string {
                     grep_open_files=true,
