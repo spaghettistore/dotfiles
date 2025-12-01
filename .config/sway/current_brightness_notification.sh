1a37fbecac74b126c630327e54c7e67d6c8216e9
spaghettistore <spaghettistore@tutamail.com>
2025-12-01 16:38:05 +0000

    Added notifications to sway keybinds. Volume: up/down/mute, Brightness: up/down, Screenshot/printscreen.

---
+++ .config/sway/current_brightness_notification.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+brightness="$(brightnessctl \
+    | grep "Current brightness:" \
+    | awk -F ":" '{print $2}' \
+    | awk '{print $2}' \
+    | tr -d "()"
+)"
+
+notify-send \
+    "Brightness" "$brightness" \
+    --urgency=low \
+    --app-name="current_brightness_notification.sh" \
+    --hint=string:x-canonical-private-synchronous:"current_brightness_notification.sh"
---
+++ .config/sway/current_volume_notification.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ "$(pactl get-sink-mute @DEFAULT_SINK@)" == "Mute: yes" ]]; then
+    volume="Muted"
+else
+    #volume="$(pactl list sinks | grep "Volume" | head -n1 | awk '{print $5}')"
+    volume="$(pactl get-sink-volume @DEFAULT_SINK@ \
+        | head -n1 \
+        | awk '{print $5}')"
+fi
+
+notify-send \
+    "Volume" "$volume" \
+    --urgency=low \
+    --app-name="current_volume_notification.sh" \
+    --hint=string:x-canonical-private-synchronous:"current_volume_notification.sh"
