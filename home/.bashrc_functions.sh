3961fa3893af6fb8c7de991a072223d10e28784a
spaghettistore <spaghettistore@tutamail.com>
2025-12-02 13:35:27 +0000

    Minor format changes and added filters to .bashrc_functions.sh

--- home/.bashrc_functions.sh
+++ home/.bashrc_functions.sh
@@ -2,9 +2,11 @@
 
 fzcd() {
     local selected
-    selected="$(find . -mindepth 1 -type d | fzf --preview 'ls {}')" \
-        && cd "$selected" \
-        || return 1
+    selected="$(find . -mindepth 1 -type d \
+        | grep -v -e "/\.steam/" -e "/\.git/" \
+        | fzf --preview 'ls {}')" \
+            && cd "$selected" \
+            || return 1
 }
 
 
@@ -15,7 +17,8 @@ fzed() {
     files=$(find . -mindepth 1 -type f)
 
     # Filter out non-text files
-    selected="$(echo "$files" | grep -v -e "/\.steam/" -e "/GIMP/2\.10/" -e "/\.config/libreoffice/" -e "\.mp3$" -e "\.wma$" -e "\.m4a$" -e "\.png$" -e "\.jpg$" -e "\.jpeg$" -e "\.svg$" -e "\.kra$" -e "\.pdf$" -e "\.epub$" -e "\.djvu$" -e "\.docx$" -e "\.mp4$" -e "\.wav$" -e "\.mmpz" -e "\.tdb$" -e "\.zip$" -e "\.7z$" -e "\.bin$" -e "\.cue$" -e "\.chd$" -e "\.iso$" -e "\.gba$" -e "\.nes$" -e "\.nds$" -e "\.srm$" -e "\.tar$" -e "\.nvmem$" -e "\.eeprom$" -e "\.ps2$" -e "/\.git/" -e "/__pycache__/" -e "a\.out" -e "\.gitignore" \
+    selected="$(echo "$files" \
+        | grep -v -e "/\.steam/" -e "/GIMP/" -e "/\.config/libreoffice/" -e "\.mp3$" -e "\.wma$" -e "\.m4a$" -e "\.png$" -e "\.jpg$" -e "\.jpeg$" -e "\.svg$" -e "\.kra$" -e "\.pdf$" -e "\.epub$" -e "\.djvu$" -e "\.docx$" -e "\.mp4$" -e "\.wav$" -e "\.mmpz" -e "\.tdb$" -e "\.zip$" -e "\.7z$" -e "\.bin$" -e "\.cue$" -e "\.chd$" -e "\.iso$" -e "\.gba$" -e "\.nes$" -e "\.nds$" -e "\.srm$" -e "\.tar$" -e "\.nvmem$" -e "\.eeprom$" -e "\.ps2$" -e "/\.git/" -e "/__pycache__/" -e "a\.out" -e "\.gitignore" \
         | sort \
         | fzf --preview 'cat {}')"
 
