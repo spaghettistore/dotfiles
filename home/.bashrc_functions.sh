5e0cf4b1ebde1390dd58ea8e6feb8edd60192e9b
spaghettistore <spaghettistore@tutamail.com>
2026-02-01 17:22:06 +0000

    Changed bashrc function fzed to have the same filters as tmux_fzed.sh

--- home/.bashrc_functions.sh
+++ home/.bashrc_functions.sh
@@ -14,16 +14,80 @@ fzed() {
     local selected=""
     local files
 
-    files=$(find . -mindepth 1 -type f)
+    if [[ "$#" -eq 1 ]]; then
+        if [[ -f "$1" ]]; then
+            # If provided argument is a file, use that and skip fzf
+            selected="$1"
+        elif [[ -d "$1" ]]; then
+            # If provided argument is a directory, open fzf using that directory
+            cd "$1" || return 1
+        fi
+    fi
 
-    # Filter out non-text files
-    selected="$(echo "$files" \
-        | grep -v -e "/\.steam/" -e "/GIMP/" -e "/\.config/libreoffice/" -e "\.mp3$" -e "\.wma$" -e "\.m4a$" -e "\.png$" -e "\.jpg$" -e "\.jpeg$" -e "\.svg$" -e "\.kra$" -e "\.pdf$" -e "\.epub$" -e "\.djvu$" -e "\.docx$" -e "\.mp4$" -e "\.wav$" -e "\.mmpz" -e "\.tdb$" -e "\.zip$" -e "\.7z$" -e "\.bin$" -e "\.cue$" -e "\.chd$" -e "\.iso$" -e "\.gba$" -e "\.nes$" -e "\.nds$" -e "\.srm$" -e "\.tar$" -e "\.nvmem$" -e "\.eeprom$" -e "\.ps2$" -e "/\.git/" -e "/__pycache__/" -e "a\.out" -e "\.gitignore" \
-        | sort \
-        | fzf --preview 'cat {}')"
+    if [[ -z "$selected" ]]; then
+        # If we are in ~, only search specific directories to reduce clutter
+        if [[ "$(pwd)" == "$HOME" ]]; then
+            files="$(find -L "$HOME/inbox" "$HOME/projects" "$HOME/refs" "$HOME/bin" \
+                "$HOME/scripts" "$HOME/dotfiles" \
+                -type f)
+    $(find -L "$HOME" -maxdepth 1 -type f)"
+        else
+            # Search recursively
+            files="$(find -L ./ -type f)"
+        fi
+
+        # Filter out non-text files
+        selected="$(echo "$files" \
+            | grep -v \
+            -e "/\.steam/" \
+            -e "/GIMP/2\.10/" \
+            -e "/\.config/libreoffice/" \
+            -e "\.mp3$" \
+            -e "\.wma$" \
+            -e "\.m4a$" \
+            -e "\.png$" \
+            -e "\.jpg$" \
+            -e "\.jpeg$" \
+            -e "\.svg$" \
+            -e "\.kra$" \
+            -e "\.pdf$" \
+            -e "\.epub$" \
+            -e "\.djvu$" \
+            -e "\.docx$" \
+            -e "\.mp4$" \
+            -e "\.wav$" \
+            -e "\.mmpz" \
+            -e "\.tdb$" \
+            -e "\.zip$" \
+            -e "\.7z$" \
+            -e "\.bin$" \
+            -e "\.cue$" \
+            -e "\.chd$" \
+            -e "\.iso$" \
+            -e "\.gba$" \
+            -e "\.nes$" \
+            -e "\.nds$" \
+            -e "\.srm$" \
+            -e "\.tar$" \
+            -e "\.nvmem$" \
+            -e "\.eeprom$" \
+            -e "\.ps2$" \
+            -e "/\.git/" \
+            -e "/__pycache__/" \
+            -e "a\.out" \
+            -e "\.gitignore" \
+            | sort \
+            | fzf --preview="batcat -n --theme=gruvbox-dark --color=always {}")"
+            #| fzf --preview 'cat {}')"
+    fi
 
     [[ -z "$selected" ]] \
-        || $EDITOR "$selected"
+        && return 1
+
+    [[ -z "$EDITOR" ]] \
+        && EDITOR="nvim"
+
+    $EDITOR "$selected"
 }
 
 
