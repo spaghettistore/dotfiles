03d2bf22a7bdf9b8cc1de31e097bc1b1d5724ba7
spaghettistore <spaghettistore@tutamail.com>
2026-04-08 16:41:12 +0100

    Changed nvim gruvbox.nvim to use 'opts' instead of 'config' as 'checkhealth' says to do

--- .config/nvim/lua/config/plugins/gruvbox.lua
+++ .config/nvim/lua/config/plugins/gruvbox.lua
@@ -1,10 +1,9 @@
 return {
     "ellisonleao/gruvbox.nvim",
     priority = 1000 ,
-    config = {
+    opts = {
         italic = {
             strings = false
         }
-    },
-    opts = ...
+    }
 }
