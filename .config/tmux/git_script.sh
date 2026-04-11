dff56e1130db5174c921b36b0554cf4eff955489
spaghettistore <spaghettistore@tutamail.com>
2026-04-11 10:35:25 +0100

    Added a git script with a confirmation prompt to be used by tmux git display-menu options 'add' and 'commit'

---
+++ .config/tmux/git_script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+get_confirmation() {
+    local message="$1"
+    local confirmation
+    read -rp "$message [y/N] " confirmation
+    case "$confirmation" in
+        [yY]*) return 0 ;;
+        *) return 1 ;;
+    esac
+}
+
+main() {
+    case "$1" in
+        "status -s") git status -s | less ;;
+        "status") git status | less ;;
+        "difftool") git difftool ;;
+        "difftool --staged") git difftool --staged ;;
+        "log --oneline") git log --oneline | less ;;
+        "log") git log | less;;
+        "add")
+            git status -s
+            if get_confirmation "Confirm 'git add .'"; then
+                git add .
+            fi
+            ;;
+        "commit")
+            git status -s
+            if get_confirmation "Confirm 'git commit'"; then
+                git commit
+            fi
+            ;;
+        *) exit 0 ;;
+    esac
+}
+
+
+main "$@"
