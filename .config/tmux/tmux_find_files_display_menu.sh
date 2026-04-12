e2f7f04d896dee20d1f0fe0413143257a2587ce9
spaghettistore <spaghettistore@tutamail.com>
2026-04-12 23:42:47 +0100

    Added what will be used in title to tmux find files display-menu

--- .config/tmux/tmux_find_files_display_menu.sh
+++ .config/tmux/tmux_find_files_display_menu.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 case "$1" in
-    "$HOME/.config/tmux/tmux_fzf_editor_new_pane.sh" | "$HOME/.config/tmux/tmux_fzed.sh") script_name="$1" ;;
+    "$HOME/.config/tmux/tmux_fzf_editor_new_pane.sh")
+        script_name="$1"
+        title_mode="split-pane"
+        ;;
+    "$HOME/.config/tmux/tmux_fzed.sh")
+        script_name="$1"
+        title_mode="new-window"
+        ;;
     *) exit 0 ;;
 esac
 
-tmux display-menu -T "Find Files" \
+tmux display-menu -T "Find Files ($title_mode)" \
     "Current Directory" "." "display-popup -w 80% -h 80% -E \"$script_name\"" \
     "Home" h "display-popup -w 80% -h 80% -E \"$script_name ~\"" \
     "Inbox" i "display-popup -w 80% -h 80% -E \"$script_name ~/inbox\"" \
