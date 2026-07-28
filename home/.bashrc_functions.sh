3a7d95091105764f7695d7d57590aabe75240e66
spaghettistore <spaghettistore@tutamail.com>
2026-07-28 23:11:58 +0100

    Added ability to use '-h' flag for help to cdi .bashrc function

--- home/.bashrc_functions.sh
+++ home/.bashrc_functions.sh
@@ -175,6 +175,16 @@ cdi() {
     [[ ! -f "$script" ]] \
         && echo "cdi: '$script': Failed to find python script" 1>&2 \
         && return 1
+
+    local opt
+    while getopts "h" opt; do
+        case "$opt" in
+            "h") "$script" -h ; return 0 ;;
+            *) echo "See 'cdi -h' for more information." >&2 ; return 1 ;;
+        esac
+    done
+    shift $((OPTIND - 1))
+
     local dir
     dir="$("$script" "$1")"
     [[ -z "$dir" ]] && return 0
