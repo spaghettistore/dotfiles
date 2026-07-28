ef7448e843dd91137bba6ea604a8c7c177821c65
spaghettistore <spaghettistore@tutamail.com>
2026-07-28 22:09:49 +0100

    Added a function 'cdi' to .bashrc that uses a python script that is not currently inside dotfiles

--- home/.bashrc_functions.sh
+++ home/.bashrc_functions.sh
@@ -168,3 +168,15 @@ cu() {
         cd "$cd_string" || return 1
     fi
 }
+
+
+cdi() {
+    local script="$HOME/resources/code/scripts/cdi/cdi.py"
+    [[ ! -f "$script" ]] \
+        && echo "cdi: '$script': Failed to find python script" 1>&2 \
+        && return 1
+    local dir
+    dir="$("$script" "$1")"
+    [[ -z "$dir" ]] && return 0
+    cd "$dir" || return 1
+}
