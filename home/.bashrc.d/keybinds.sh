e11bfa027fa93dc5e93aa8f76ce9eaca5001a163
spaghettistore <spaghettistore@tutamail.com>
2026-07-30 18:11:20 +0100

    Made .bashrc ctrl+f open in current by default

--- home/.bashrc.d/keybinds.sh
+++ home/.bashrc.d/keybinds.sh
@@ -6,6 +6,6 @@
 #bind '"\e[B": history-search-forward'
 
 # Script keybinds (with vim mode this only works in insert mode)
-bind -x '"\C-F": "~/.config/tmux/tmux_fzf_file_picker.sh -o window"'
+bind -x '"\C-F": "~/.config/tmux/tmux_fzf_file_picker.sh -o current"'
 bind -x '"\C-G": ". ~/.config/tmux/tmux_fzf_directory_picker.sh"'
 bind -x '"\C-O": ". ~/.config/tmux/tmux_files_and_directory_picker.sh"'
