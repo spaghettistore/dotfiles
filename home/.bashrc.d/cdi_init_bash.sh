0b61f1fbdb7630258386eb722a55c34cea62340b
spaghettistore <spaghettistore@tutamail.com>
2026-08-13 15:16:53 +0100

    Removed the bash getopts so the python getopt can be used, and using '--' works as expected

--- home/.bashrc.d/cdi_init_bash.sh
+++ home/.bashrc.d/cdi_init_bash.sh
@@ -1,19 +1,11 @@
 cdi() {
     local script="change_directory_interactively.py"
-    local opt
-    while getopts "h" opt; do
-        case "$opt" in
-            "h") "$script" -h ; return 0 ;;
-            *) echo "See 'cdi -h' for more information." >&2 ; return 1 ;;
-        esac
-    done
-    shift $((OPTIND - 1))
-
     local response
+
     response="$("$script" "$@")"
     [[ -z "$response" ]] && return 0
 
-    # Only attempt to cd if the python script outputs one line: 'cd DIR'
+    # Only attempt to cd if the python script outputs one line that starts with 'cd'
     if [[ "$(echo "$response" | awk '{print $1}')" == "cd" ]]; then
         local dir
         # Print all but $1
